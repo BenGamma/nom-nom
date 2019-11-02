2d7b1f078430a778e966ba6d9e9960e6fe6b0240
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-11-02 17:36:09 +0100

    [ci skip]

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.__precacheManifest = [
     "url": "commons.2d1e0acea8dceab45e92.css"
   },
   {
-    "url": "commons-981e47a03cd0da70a854.js"
+    "url": "commons-ddd67bbc5f9a63b06530.js"
   },
   {
     "url": "app-d7bcc77683d6eaa8dfc9.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "86a0bf5d613eeded50dc2287a32a1176"
+    "revision": "24d5d70efd0b1132b5e1e43f192a742f"
   },
   {
     "url": "manifest.webmanifest",
